0f6970a927d6e9e1066bf4aabe07586704ff8ab4
Siarhei Maliauka <small1803@sam-solutions.net>
2020-10-02 13:18:55 +0300

    Prepare solutions for support Visual Studio 2019

--- runs-msbuild.bat
+++ runs-msbuild.bat
@@ -4,9 +4,10 @@ set BUILDTYPE="%~1"
 set ACTION="%~2"
 
 for %%s in (
-    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe"
 ) do (
     if exist %%s (
         echo %%s build.proj %ACTION% %BUILDTYPE%
@@ -16,7 +17,7 @@ for %%s in (
 )
 
 :notfound
-echo Could not find MSBuild.exe. Make sure Visual Studio 2017 is installed and try again.
+echo Could not find MSBuild.exe. Make sure Visual Studio 2019 is installed and try again.
 
 :done
 pause
